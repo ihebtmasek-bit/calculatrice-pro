3d8a3069315caba79cdca69e6fefc10846619d2b
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-24 22:07:21 +0100

    Calculator Pro - Première version

--- app/src/main/java/com/calculatorpro/app/MainActivity.java
+++ app/src/main/java/com/calculatorpro/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.calculatorpro.app;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         
         WebView webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("file:///android_asset/calculator.html");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
         
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        
+        webView.loadUrl("file:///android_asset/calculator.html");
         setContentView(webView);
     }
 }
